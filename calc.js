57eb4ad3c5a62f6ea7d6329d2e54fd74ed76c198
Dallin Harkness <dalhark99@gmail.com>
2021-11-09 15:47:37 -0700

    Added a place for a third number

--- calc.js
+++ calc.js
@@ -13,19 +13,21 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
 
 	console.log('mathSymbol', mathSymbol)
 	console.log('num1', num1)
 	console.log('num2', num2)
+	console.log('num3', num3)
 
 	if (mathSymbol === "+"){
-		console.log(num1 + num2)
+		console.log(num1 + num2 + num3)
 	}
 	else if(mathSymbol === "-"){
-		console.log(num1 - num2)
+		console.log(num1 - num2 - num3)
 	}
 	else if(mathSymbol === "/"){
-		console.log(num1 / num2)
+		console.log(num1 / num2 / num3)
 	}
 	else if(mathSymbol === "Sqrt"){
 		console.log(Math.sqrt(num1))
@@ -40,7 +42,7 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(Math.exp(num1))
 	}
 	else if (mathSymbol === "modulus"){
-		console.log(num1 % num2)
+		console.log(num1 % num2 % num3)
 	}
 
 
