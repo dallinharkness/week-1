d06003c185186817cc98f1cfc137855cbcce1ab9
Dallin Harkness <dalhark99@gmail.com>
2021-11-10 15:15:16 -0700

    Finished more problems

--- lab.js
+++ lab.js
@@ -78,7 +78,9 @@ let rain = true
   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
 */
 
-//CODE HERE
+// for (let i = 0; i <10; i++){
+//   console.log("Hello!")
+// }
 
 
 ////////// PROBLEM 6 //////////
@@ -87,7 +89,9 @@ let rain = true
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
+// for(let i = 0; i < 11; i++){
+//   console.log(i)
+// }
 
 
 ////////// PROBLEM 7 //////////
