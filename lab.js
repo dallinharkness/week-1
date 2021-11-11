68cc98b7034ddffe1c2158dda9ca0329ac66e947
Dallin Harkness <dalhark99@gmail.com>
2021-11-11 14:07:44 -0700

    inital commit

--- lab.js
+++ lab.js
@@ -127,17 +127,17 @@ let passingScore = 7
 
 */
 
- let changeMyMind = true
-changeMyMind = !changeMyMind
-console.log(changeMyMind)
-changeMyMind = !changeMyMind
-console.log(changeMyMind)
+//  let changeMyMind = true
+// changeMyMind = !changeMyMind
+// console.log(changeMyMind)
+// changeMyMind = !changeMyMind
+// console.log(changeMyMind)
 
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
-changeMyMind != changeMyMind
-console.log(changeMyMind)
+// changeMyMind != changeMyMind
+// console.log(changeMyMind)
 
 
 
@@ -149,14 +149,15 @@ let z = 5
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 
-for (let z = 5; z > 0; z--){
-    console.log(z)
-  } for (let z = 4; z >0; z--){
-    console.log(z)
-  } for (let z = 3; z >0; z--){
-    console.log(z)}
-    for (let z = 2; z >0; z--){
-      console.log(z)}
-      for (let z = 1; z >0; z--){
-        console.log(z)
-      }
+// for (let z = 5; z > 0; z--){
+//     console.log(z)
+//   } for (let z = 4; z >0; z--){
+//     console.log(z)
+//   } for (let z = 3; z >0; z--){
+//     console.log(z)}
+//     for (let z = 2; z >0; z--){
+//       console.log(z)}
+//       for (let z = 1; z >0; z--){
+//         console.log(z)
+//       }
+
